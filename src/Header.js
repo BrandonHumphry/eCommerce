31424117f3a54b3b128735b750507aa0b720b7e7
Brandon Humphry <brandonhumphry@gmail.com>
2020-11-26 23:46:47 -0800

    file cleanup and header setup

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <img
+        className="header__logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+      />
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+      </div>
+      <div className="header__nav"></div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBnc96ptJ3bp_slbM5lMQ86HkmLOrLI0vU",
+  authDomain: "ecommerce-3f5ca.firebaseapp.com",
+  databaseURL: "https://ecommerce-3f5ca.firebaseio.com",
+  projectId: "ecommerce-3f5ca",
+  storageBucket: "ecommerce-3f5ca.appspot.com",
+  messagingSenderId: "770035716514",
+  appId: "1:770035716514:web:f2087c68f7460d876762fb",
+  measurementId: "G-9XSG4Z6ZFB"
+};
