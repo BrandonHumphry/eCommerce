2c70c83dc44b15bc10d23dae8849abd8aeec3f02
Brandon Humphry <brandonhumphry@gmail.com>
2020-11-30 22:43:45 -0800

    styling product card with image

--- src/Product.js
+++ src/Product.js
@@ -2,7 +2,25 @@ import React from "react";
 import "./Product.css";
 
 function Product() {
-  return <div className="product"></div>;
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>The lean startup</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>19.99</strong>
+        </p>
+        <div className="product__rating">
+          <p>star</p>
+        </div>
+      </div>
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400.jpg"
+        alt=""
+      />
+      <button>Add to Basket</button>
+    </div>
+  );
 }
 
 export default Product;
