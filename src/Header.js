635508ee67c01179d280f8e13648c84509f0881a
Brandon Humphry <brandonhumphry@gmail.com>
2020-12-05 23:09:02 -0800

    working on checkout design and layout with currency

--- src/Header.js
+++ src/Header.js
@@ -2,15 +2,19 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasket from "@material-ui/icons/ShoppingBasket";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
-      <img
-        className="header__logo"
-        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-        alt="amazon logo in white text with orange arrow under"
-      />
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt="amazon logo in white text with orange arrow under"
+        />
+      </Link>
+
       <div className="header__search">
         <input type="text" className="header__searchInput" />
         <SearchIcon className="header__searchIcon" />
@@ -30,16 +34,17 @@ function Header() {
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
         </div>
-
-        <div className="header__optionBasket">
-          <ShoppingBasket />
-          <span
-            className="header__optionLineTwo           
+        <Link to="/checkout">
+          <div className="header__optionBasket">
+            <ShoppingBasket />
+            <span
+              className="header__optionLineTwo           
             header__basketCount"
-          >
-            0
-          </span>
-        </div>
+            >
+              0
+            </span>
+          </div>
+        </Link>
       </div>
     </div>
   );
