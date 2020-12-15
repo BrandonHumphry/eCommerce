46cfda8df8a03e6114f4ce6fb1ea44167fd734ad
Brandon Humphry <brandonhumphry@gmail.com>
2020-12-14 23:19:52 -0800

    built and styled login page

--- src/App.js
+++ src/App.js
@@ -5,18 +5,24 @@ import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import { Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
+import Login from "./Login";
 
 function App() {
   return (
     <Router>
       <div className="app">
-        {/* placing the header here outside of the switch renders it regardless of page you are on */}
-        <Header />
+        {/* placing the header here outside of the switch because it should be on every page */}
+        {/* <Header /> */}
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
