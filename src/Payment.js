742c10ea1b2211ababe5d828f6da6883eb9b67fd
Brandon Humphry <brandonhumphry@gmail.com>
2020-12-22 23:29:54 -0800

    styling for Checkout complete

--- src/Payment.js
+++ src/Payment.js
@@ -2,39 +2,46 @@ import React from "react";
 import "./Payment.css";
 import { useStateValue } from "./StateProvider";
 import CheckoutProduct from "./CheckoutProduct";
+import { Link } from "react-router-dom";
 
 function Payment() {
   const [{ basket, user }, dispatch] = useStateValue();
   return (
     <div className="payment">
-      <div className="payment__Container">
+      <div className="payment__container">
+        <h1>
+          Checkout (<Link to="/checkout">{basket.length}items</Link>)
+        </h1>
+
         {/* payment section - delivery address */}
         <div className="payment__section">
           <div className="payment__title">
             <h3>Delivery Address</h3>
-            <div className="payment__address">
-              <p>{user?.email}</p>
-              <p>123 react lane</p>
-              <p>Orange,CA</p>
-            </div>
+          </div>
+          <div className="payment__address">
+            <p>{user?.email}</p>
+            <p>123 react lane</p>
+            <p>Orange,CA</p>
           </div>
         </div>
         {/* payment section - review items */}
-        <div className="payment__section"></div>
-        <div className="payment__title">
-          <h3>Review items and delivery</h3>
-        </div>
-        <div className="payment__items">
-          {/* products in basket will show here */}
-          {basket.map(item => (
-            <CheckoutProduct
-              id={item.id}
-              title={item.title}
-              image={item.image}
-              price={item.price}
-              rating={item.rating}
-            />
-          ))}
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review items and delivery</h3>
+          </div>
+
+          <div className="payment__items">
+            {/* products in basket will show here */}
+            {basket.map(item => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
         </div>
         {/* payment section - payment method */}
         <div className="payment__section">
